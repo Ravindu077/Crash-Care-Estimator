951007fa8b1e62023dae6e0e7d291d08ddb12d31
Ravindu Gunawickrama <103891464+Ravindu077@users.noreply.github.com>
2024-12-31 23:11:38 +0530

    Update firebase.js

--- screens/Firebase/firebase.js
+++ screens/Firebase/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyALsyegVltTIc33lGaBQBOyR7Xe4VKCJB0",
-  authDomain: "crashcareestimator2024.firebaseapp.com",
-  projectId: "crashcareestimator2024",
-  storageBucket: "crashcareestimator2024.appspot.com",
-  messagingSenderId: "116373921680",
-  appId: "1:116373921680:web:fbbe3e4a66aa1e827c9d15",
-  measurementId: "G-7SC5KXL533"
+  apiKey: "Your Api key",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
